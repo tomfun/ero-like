05ea4728b9436b5900b2a24584c2d9b4e5d7a7a9
Stas Bsov <ilya.pojidaev@yandex.ua>
2021-05-07 04:16:45 +0300

    set_new_comp_set_fetch_data

---
+++ my-vue3-app/src/components/ReportDB.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <SingleReport
+      v-for="item in data.items"
+      v-bind:key="item.id"
+      v-bind:item="item"
+    ></SingleReport>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import SingleReport from './SingleReport.vue';
+
+export default defineComponent({
+  name: 'ReportDB',
+  props: {
+    msg: String,
+  },
+  components: {
+    SingleReport,
+  },
+  data() {
+    return {
+      data: {
+        items: [],
+      },
+    };
+  },
+  beforeMount() {
+    this.getName();
+  },
+  methods: {
+    async getName() {
+      const res = await fetch('http://localhost:3001/api/report');
+      const data = await res.json();
+      this.data = data;
+    },
+  },
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h1 {
+  margin: 40px 0 0;
+  font-size: 48px;
+}
+</style>
--- my-vue3-app/src/router/index.ts
+++ my-vue3-app/src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
 import Home from '../views/Home.vue';
 import Submit from '../views/Submit.vue';
+import Reports from '../views/Reports.vue';
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -12,6 +13,11 @@ const routes: Array<RouteRecordRaw> = [
     path: '/submit',
     name: 'Submit',
     component: Submit,
+  },
+  {
+    path: '/reports',
+    name: 'Reports',
+    component: Reports,
   },
   {
     path: '/about',
