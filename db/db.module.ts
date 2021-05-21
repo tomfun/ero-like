c66297ea4d845eb4e336b7761cc8a836e9d15c37
tomfun <tomfun1990@gmail.com>
2021-05-21 21:11:57 +0300

    clear changes

--- db/db.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Document } from './entity/Document';
-
-@Module({
-  imports: [TypeOrmModule.forFeature([Document])],
-  exports: [TypeOrmModule],
-})
-export class DbModule {}
