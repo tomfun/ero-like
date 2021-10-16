6e4b23bafa83a034cb7426b9d6cd9f57ce10bed5
Grigory <tomfun1990@gmail.com>
2021-10-16 22:10:38 +0300

    fix any

--- my-vue3-app/src/store/reports/actions.ts
+++ my-vue3-app/src/store/reports/actions.ts
@@ -1,3 +1,4 @@
+import { ActionContext } from 'vuex';
 import api from '@/services/api';
 import { Pagination } from './state';
 import { SET_DATA, SET_PAGINATION } from './mutations';
@@ -5,7 +6,7 @@ import { SET_DATA, SET_PAGINATION } from './mutations';
 export const FETCH_REPORTS = 'load_reports';
 
 export default {
-  async [FETCH_REPORTS]({ commit }: any, payload: Pagination) {
+  async [FETCH_REPORTS]({ commit }: ActionContext<unknown, unknown>, payload: Pagination) {
     console.log(payload);
     const data = await api.fetchReports(
       payload.page,
