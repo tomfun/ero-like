12954f093aeae90962af371be52a1e41584d8ce2
tomfun <tomfun1990@gmail.com>
2023-02-12 11:16:09 +0200

    nvm commander

--- dev/start.js
+++ dev/start.js
@@ -1,11 +1,18 @@
+const { resolve } = require('path');
 const commander = require('node-shell-commandor');
 
 commands = [{
   command: 'docker-compose up'
 }, {
-  command: 'cd api && npm run start:dev'
+  command: 'export NVM_DIR="$HOME/.nvm"; [ -s "$NVM_DIR/nvm.sh" ] && \\. "$NVM_DIR/nvm.sh"; nvm use && npm run start:dev',
+  opt: {
+    cwd: resolve(__dirname, '..', 'api'),
+  }
 }, {
-  command: 'cd my-vue3-app && npm run serve'
+  command: 'export NVM_DIR="$HOME/.nvm"; [ -s "$NVM_DIR/nvm.sh" ] && \\. "$NVM_DIR/nvm.sh"; nvm use && npm run serve',
+  opt: {
+    cwd: resolve(__dirname, '..', 'my-vue3-app'),
+  }
 }];
 
 commander.run(commands);
