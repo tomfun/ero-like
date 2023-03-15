fc0dde02000d757c82ce0eb424cade96d1953441
tomfun <tomfun1990@gmail.com>
2023-03-15 20:28:17 +0200

    fixes

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,25 +1,24 @@
 import { NestFactory } from '@nestjs/core';
-import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import * as process from 'process';
+import * as querystring from 'querystring';
 import { ReportModule } from './report.module';
 import Server from 'fastify';
-import fastifyRawBody from 'fastify-raw-body';
 
 async function bootstrap() {
-  const querystring = require('querystring');
   const server = Server({
     // @see https://www.fastify.io/docs/latest/Reference/Server/#trustproxy
     trustProxy: +process.env.TRUST_PROXY,
     logger: true,
-    querystringParser: str => querystring.parse(),
+    querystringParser: (str) => querystring.parse(str),
   });
-  server.register(fastifyRawBody, {
-    global: false, // add the rawBody NOT to every request. **Default true**
-    // encoding: false, //Buffer; **Default utf8**
-    runFirst: true, // get the body before any preParsing hook change/uncompress it. **Default false**
-    routes: ['/webhook/:gw'],
-  });
-  const app = await NestFactory.create<NestFastifyApplication>(ReportModule, new FastifyAdapter(server));
+  const app = await NestFactory.create<NestFastifyApplication>(
+    ReportModule,
+    new FastifyAdapter(server),
+  );
   await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
