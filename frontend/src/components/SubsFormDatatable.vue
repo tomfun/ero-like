6778bfe583f0504986cbafebbfa456c25dabaf56
Ilya Pojidayev <ilya.pozh@gmail.com>
2023-05-07 20:45:20 +0300

    submit response validation

---
+++ frontend/src/components/SubsFormDatatable.vue
@@ -0,0 +1,33 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+// type Substance = {
+//   timeSecond: number;
+//   dose: number;
+//   doseUnit: string;
+//   namePsychonautWikiOrg: string;
+//   routeOfAdministration: string;
+//   activeSubstance: string;
+//   surePercent: number;
+//   dataCompleted: () => void;
+// };
+
+// type TimeLineReport = {
+//   timeSecond: number;
+//   report: string;
+//   dataCompleted: Function;
+//   simple: boolean;
+// };
+
+// type ReportData = {
+//   substances: Substance[];
+//   timeLineReport: TimeLineReport[];
+//   background: string;
+//   dateTimestamp: number;
+//   title: string;
+// };
+export default defineComponent({
+  name: 'SubFormTable',
+});
+
+</script>
