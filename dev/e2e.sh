4a895fe886b34502e868769d444748dae5c3855a
tomfun <tomfun1990@gmail.com>
2023-05-13 19:56:18 +0300

    fix e2e ci

--- dev/e2e.sh
+++ dev/e2e.sh
@@ -2,7 +2,7 @@
 
 export uid=$(id -u)
 export gid=$(id -g)
-export compose="docker compose --file docker-compose.ci.yml --file docker-compose.ci-local.yml --project-name ero-like-$CI_PIPELINE_ID-$CI_JOB_ID"
+export compose="docker compose --file docker-compose.ci.yml --project-name ero-like-$CI_PIPELINE_ID-$CI_JOB_ID"
 export npm="$compose run --rm api npm"
 export migrate="$npm run migrate"
 
